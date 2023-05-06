cbf157749df6fd8064c3cfd25ae5b3cf219e145f
Anastasia Migaeva <pandora1983@yandex.ru>
2023-05-06 13:33:35 +0300

    добавила стейт для ховера на карточку фильма

--- project/src/components/movie-list/movie-list.tsx
+++ project/src/components/movie-list/movie-list.tsx
@@ -1,17 +1,22 @@
 import MovieCard from '../movie-card/movie-card';
 import {IMovie} from '../../types/Movie';
+import {useState} from 'react';
 
 interface FilmsListProps {
   filmsAmount: number;
   movies: IMovie[];
 }
 
-const MovieList = ({filmsAmount, movies}: FilmsListProps):JSX.Element => (
-  <div className="catalog__films-list">
-    {
-      movies.slice(0, filmsAmount).map((movie) => <MovieCard key={movie.id} movie={movie} />)
-    }
-  </div>
-);
+const MovieList = ({filmsAmount, movies}: FilmsListProps):JSX.Element => {
+  const [, setActiveCardId] = useState<number | null>(null);
+
+  return (
+    <div className="catalog__films-list">
+      {
+        movies.slice(0, filmsAmount).map((movie) => <MovieCard key={movie.id} movie={movie} onActiveCardId={setActiveCardId} />)
+      }
+    </div>
+  )
+};
 
 export default MovieList;

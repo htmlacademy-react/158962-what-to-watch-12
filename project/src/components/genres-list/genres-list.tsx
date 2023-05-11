0198dad23eba0bdbfcc58294c26ac91e8c4435a4
Anastasia Migaeva <pandora1983@yandex.ru>
2023-05-11 18:27:58 +0300

    переделала логику с кнопкой

--- project/src/components/genres-list/genres-list.tsx
+++ project/src/components/genres-list/genres-list.tsx
@@ -1,7 +1,7 @@
 import {useAppDispatch} from '../../hooks';
 import cn from 'classnames';
 import {Link} from 'react-router-dom';
-import {changeGenre} from '../../store/slices/films-slice/films-slice';
+import {changeGenre, resetFilmsCount} from '../../store/slices/films-slice/films-slice';
 import {DEFAULT, MAX_GENRES_AMOUNT} from '../../const';
 
 interface GenresListProps {
@@ -12,19 +12,32 @@ interface GenresListProps {
 const GenresList = ({currentGenre, genresList}: GenresListProps):JSX.Element => {
   const dispatch = useAppDispatch();
 
+  const handleGenreLinkClick = (link: string, currentGenre: string): boolean | void => {
+    if ( link === currentGenre) {
+      return false;
+    }
+    dispatch(changeGenre(link));
+    dispatch(resetFilmsCount());
+  };
+
   return (
     <ul className="catalog__genres-list">
-      {[DEFAULT, ...genresList].slice(0, MAX_GENRES_AMOUNT).map((name) => (
-        <li key={name} className={cn('catalog__genres-item', currentGenre === name && 'catalog__genres-item--active')}>
-          <Link to="/#"
-            onClick={(evt) => {
-              evt.preventDefault();
-              dispatch(changeGenre(name));}}
-            className="catalog__genres-link"
-          >{name}
-          </Link>
-        </li>
-      ))}
+      {[DEFAULT, ...genresList].slice(0, MAX_GENRES_AMOUNT).map((name) => {
+
+        return (
+          <li key={name} className={cn('catalog__genres-item', currentGenre === name && 'catalog__genres-item--active')}>
+            <Link to="/#"
+                  onClick={(evt) => {
+                    evt.preventDefault();
+                    handleGenreLinkClick(name, currentGenre);
+                    }
+                  }
+                  className="catalog__genres-link"
+            >{name}
+            </Link>
+          </li>
+        )
+      })}
     </ul>
   );
 };
--- project/src/components/show-more-button/show-more-button.tsx
+++ project/src/components/show-more-button/show-more-button.tsx
@@ -1,15 +1,20 @@
-interface ShoMoreButtonProps {
-  onButtonShowMoreClick: () => void;
-}
+import {useAppDispatch} from '../../hooks';
+import { showMoreFilms} from '../../store/slices/films-slice/films-slice';
 
-const ShowMoreButton = ({onButtonShowMoreClick}: ShoMoreButtonProps):JSX.Element => (
-  <div className="catalog__more">
-    <button className="catalog__button"
-      onClick={onButtonShowMoreClick}
-      type="button"
-    >Show more
-    </button>
-  </div>
-);
+const ShowMoreButton = ():JSX.Element => {
+  const dispatch = useAppDispatch();
+
+  const handleShowMoreButtonClick = () => dispatch(showMoreFilms());
+
+  return (
+    <div className="catalog__more">
+      <button className="catalog__button"
+              onClick={handleShowMoreButtonClick}
+              type="button"
+      >Show more
+      </button>
+    </div>
+  )
+};
 
 export default ShowMoreButton;
